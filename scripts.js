76bf482744aec49f282cb140d2ab606aa4c481fb
Ankur Choudhury <113258352+ankurchoudhury1@users.noreply.github.com>
2022-10-29 20:53:18 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,7 +6,18 @@ $("#play-btn").click(() => {
     }
 });
 
-$("#tab-works-links").click(() => {
-    $("#works").toggle();
-    $("#links").toggle();
+$("#tab-works").click(() => {
+    $("#works").show();
+    $("#links").hide();
+    $("#tab-works").css({ color: "#404040" });
+    $("#tab-links").css({ color: "#b5b5b5" });
+    $("#tab-blogs").css({ color: "#b5b5b5" });
+});
+
+$("#tab-links").click(() => {
+    $("#works").hide();
+    $("#links").show();
+    $("#tab-works").css({ color: "#b5b5b5" });
+    $("#tab-links").css({ color: "#404040" });
+    $("#tab-blogs").css({ color: "#b5b5b5" });
 });
